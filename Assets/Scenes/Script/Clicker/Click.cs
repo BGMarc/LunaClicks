8f594a681ce324cd503150e84a196a546a670a05
BGMarc <36140780+BGMarc@users.noreply.github.com>
2019-08-15 04:49:33 +1000

    Create Click.cs

---
+++ Assets/Scenes/Script/Clicker/Click.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public static int clickValue;
+    public static int amountPerSecond;
+    private void Start()
+    {
+        //sets the initial click value to 1
+        clickValue = 1;
+    }
+    public void ClickerButton()
+    {
+        //makes the clicker button change both the score and the high score (for the moon)
+        ScoreManager.score+=clickValue;
+        ScoreManager.HighScore += clickValue;
+        ScoreManager.Increase();
+    }
+    public void Update()
+    {
+        // creates a hotkey to press the button when you press thew space button
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ScoreManager.score += clickValue;
+            ScoreManager.HighScore += clickValue;
+            ScoreManager.Increase();
+        }
+    }
+
+
+}

be398464d47b35b8f2c413517c37752e0d8ad080
BGMarc <36140780+BGMarc@users.noreply.github.com>
2019-08-15 04:50:07 +1000

    Create SpriteGenerator.cs

---
+++ Assets/Scenes/Script/Clicker/SpriteGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteGenerator : MonoBehaviour
+{
+    // <access-specifier> <data-type> [] <variable-name>;
+    public GameObject spritePrefab;
+    //desiginates the sprite as an array that can be changed
+    public Sprite[] sprites;
+    // desiginates the spawnPos as an array that can be changed
+    public Transform[] spawnPos;
+    //int randomIndex;
+
+    // Update is called once per frame
+    public void SpawnSprite(int index)
+    {
+        int randomIndex = Random.Range(0, 72);
+        //spawns the sprite into the world
+        GameObject clone = Instantiate(spritePrefab);
+        SpriteRenderer spriteRenderer = clone.GetComponent<SpriteRenderer>();
+        //chooses a sprite from the array
+        spriteRenderer.sprite = sprites[index];
+        clone.transform.position = spawnPos[Random.Range(0, 72)].position;
+
+        // Spawn Prefab with Sprite Component
+        // Get Sprite Component
+        // Set component.sprite's value to sprites[index]
+       // Instantiate(Sprite[], new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+    }
+}
